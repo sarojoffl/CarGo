b85ea44363486d0cbce806a0e4222fa7ecc6c0db
Saroj <sarojoffl@gmail.com>
2024-06-22 15:16:52 +0545

    Fix settings.py path handling with pathlib

--- CarService/CarService/settings.py
+++ CarService/CarService/settings.py
@@ -10,11 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -77,7 +76,7 @@ WSGI_APPLICATION = 'CarService.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -119,10 +118,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'sales_rental', 'static'),
+    BASE_DIR / 'sales_rental' / 'static',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,3 +143,4 @@ EMAIL_HOST_USER = 'cargonepal2024@gmail.com'
 EMAIL_HOST_PASSWORD = 'wbahoiftynednnig'
 
 AUTH_USER_MODEL = 'sales_rental.User'
+
